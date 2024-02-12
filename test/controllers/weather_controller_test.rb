1fe2105736d2442b9e823cc40d17c51579aa8c8e
turtle-coder-woder <turtlecoderwoder@gmail.com>
2024-02-12 13:34:59 +0530

    removing unwanted test files

--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class WeatherControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
   end
